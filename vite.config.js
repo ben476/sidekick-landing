a4a27d6bfbc463f553dfe46758b04132ed051a82
Benjamin Hong <benjamin.hong476@gmail.com>
2024-01-30 23:53:13 +1300

    Add screenshot

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "npm:vite";
+
+export default defineConfig({
+    server: {
+        proxy: {
+            "/api/search": "http://localhost:8001",
+            // "/api/search": "https://search.benhong.me",
+            // "/api/chat": "http://localhost:8000",
+            // "/api/messages": "http://localhost:8000",
+            "/api": "http://localhost:8000",
+        },
+    }
+});
